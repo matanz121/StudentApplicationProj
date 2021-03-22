b83e9bd95bda68d10f377cd8b6e8fda1d884fe33
Nati Abutbul <Netanel.Abutbul@cognyte.com>
2021-03-22 23:25:08 +0200

    remove unnecessary files + add logo

--- StudentsApplicationProj/Shared/Models/BaseAPIResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace StudentsApplicationProj.Shared.Models
-{
-    public class BaseAPIResponse
-    {
-        public string Message { get; set; }
-        public bool IsSuccess { get; set; }
-    }
-}
